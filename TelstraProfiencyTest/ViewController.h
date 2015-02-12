722de87bd9441652dc4d26a1c936e9b080d30933
Avishek <avishek.banerjee3@cognizant.com>
2015-02-12 17:26:10 +1100

    Initial Commit

---
+++ TelstraProfiencyTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TelstraProfiencyTest
+//
+//  Created by Avishek Banerjee on 12/02/2015.
+//  Copyright (c) 2015 Cognizant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TelstraProfiencyTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TelstraProfiencyTest
+//
+//  Created by Avishek Banerjee on 12/02/2015.
+//  Copyright (c) 2015 Cognizant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
